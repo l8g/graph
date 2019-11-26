f976736bdd9cbdfa9c814f2de8ecff43225f0bf2
lidongdong <lidongdong_xynu@163.com>
2019-11-26 12:40:24 +0800

    dfs path

---
+++ Graph/Expression/Path.py
@@ -0,0 +1,50 @@
+﻿
+
+from Graph import Graph
+
+class Path():
+    
+    def __init__(self, g : Graph, s, t):
+        self._g = g
+        self._s = s;
+        self._t = t;
+
+        self._visited = [False] * g.v()
+        self._pre = [-1] * g.v()
+        self._pre[s] = s
+        self._dfs(s)
+
+    def _dfs(self, u):
+        self._visited[u] = True
+        if u == self._t:
+            return True
+        for v in self._g.adj(u):
+            if not self._visited[v]:
+                self._pre[v] = u
+                if self._dfs(v):
+                    return True
+        return False
+
+    def connected(self):
+        return self._visited[self._t]
+
+    def path(self):
+        res = []
+        if not self.connected():
+            return res
+        curr = self._t
+        while curr != self._s:
+            res.insert(0, curr)
+            curr = self._pre[curr]
+        res.insert(0, self._s)
+        print(self._visited)
+        return res
+
+    
+if __name__ == "__main__":
+    g = Graph("g.txt")
+    path = Path(g, 0, 5)
+    print("0->5: ", path.path())
+
+    path2 = Path(g, 0, 1)
+    print("0->5: ", path2.path())
---
+++ Graph/Expression/SingleSourcePath.py
@@ -0,0 +1,45 @@
+﻿# -*- coding: utf-8 -*-
+
+from Graph import Graph
+
+class SingleSourcePath():
+    
+    def __init__(self, g : Graph, s : int):
+        self._g = g
+        self._s = s;
+        self._g.validate(s)
+
+        self._visited = [False] * g.v()
+        self._pre = [-1] * g.v()
+
+        self._pre[s] = s
+        self._dfs(self._s)
+
+    def _dfs(self, u):
+        self._visited[u] = True
+        for v in self._g.adj(u):
+            if not self._visited[v]:
+                self._pre[v] = u
+                self._dfs(v)
+
+    def isConnectedTo(self, t):
+        self._g.validate(t)
+        return self._visited[t]
+
+    def path(self, t):
+        res = []
+        if not self.isConnectedTo(t):
+            return res
+        cur = t
+        while cur != self._s:
+            res.insert(0, cur)
+            cur = self._pre[cur]
+        res.insert(0, self._s)
+        return res
+
+
+if __name__ == "__main__":
+    g = Graph("g.txt")
+    sspath = SingleSourcePath(g, 0)
+    print("0->5: ", sspath.path(5))
+    print("0->4: ", sspath.path(4))
