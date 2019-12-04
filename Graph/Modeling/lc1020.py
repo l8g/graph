f857898465e2e147f07f63b8c59f786f133e04bf
lidongdong <lidongdong_xynu@163.com>
2019-12-04 22:19:20 +0800

    886

---
+++ Graph/Modeling/lc1020.py
@@ -0,0 +1,55 @@
+﻿class Solution:
+
+    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+    def numEnclaves(self, A) -> int:
+        self._r = len(A)
+        if self._r == 0:
+            return 0
+        self._c = len(A[0])
+        if self._c == 0:
+            return 0
+
+        self._visited = [[False for _ in range(self._c)] for _ in range(self._r)]
+        self._A = A
+        count = 0
+        for i in range(self._r):
+            for j in range(self._c):
+                if not self._visited[i][j] and A[i][j] == 1:
+                    count += self._dfs(i, j)
+        return count
+
+    def _dfs(self, i, j):
+        self._visited[i][j] = True
+
+        count = 0 if self._atEdge(i, j) else 1
+        for d in Solution.dirs:
+            nx, ny = i + d[0], j + d[1]
+            if self._inArea(nx, ny) and not self._visited[nx][ny] and self._A[nx][ny] == 1:
+                childCount = self._dfs(nx, ny)
+                if childCount == 0 or count == 0:
+                    count = 0
+                else:
+                    count += childCount
+        return count
+
+        
+    def _inArea(self, x, y):
+        return x >= 0 and x < self._r and y >= 0 and y < self._c
+
+    def _atEdge(self, x, y):
+        return x == 0 or x == self._r - 1 or y == 0 or y == self._c - 1
+
+s = Solution()
+
+
+print(s.numEnclaves([[0,0,0,1,1,1,0,1,0,0],
+                     [1,1,0,0,0,1,0,1,1,1],
+                     [0,0,0,1,1,1,0,1,0,0],
+                     [0,1,1,0,0,0,1,0,1,0],
+                     [0,1,1,1,1,1,0,0,1,0],
+                     [0,0,1,0,1,1,1,1,0,1],
+                     [0,1,1,0,0,0,1,1,1,1],
+                     [0,0,1,0,0,1,0,1,0,1],
+                     [1,0,1,0,1,1,0,0,0,0],
+                     [0,0,0,0,1,1,0,0,0,1]]))
---
+++ Graph/Modeling/lc886.py
@@ -0,0 +1,30 @@
+﻿class Solution:
+    def possibleBipartition(self, N: int, dislikes) -> bool:
+        if N < 3:
+            return True
+        g = [set() for _ in range(N)]
+        for l in dislikes:
+            g[l[0] - 1].add(l[1] - 1)
+            g[l[1] - 1].add(l[0] - 1)
+
+        self._g = g
+        self._color = [-1] * N
+        self._visited = [False] * N
+        for i in range(N):
+            if not self._visited[i] and not self._dfs(i, 0):
+                return False
+        return True
+
+    def _dfs(self, v, color):
+        self._visited[v] = True
+        self._color[v] = color
+        for w in self._g[v]:
+            if not self._visited[w]:
+                if not self._dfs(w, 1 - color):
+                    return False
+            elif self._color[w] == self._color[v]:
+                return False
+        return True
+
+s = Solution()
+s.possibleBipartition(4, [[1,2],[1,3],[2,4]])
