f9f4acd78f1f71362c629de6435ad45f0a5f05f1
lidongdong <lidongdong_xynu@163.com>
2019-12-04 19:54:22 +0800

    733 547

---
+++ Graph/Modeling/lc547.py
@@ -0,0 +1,23 @@
+﻿class Solution:
+    def findCircleNum(self, M) -> int:
+        self._n = len(M)
+        if self._n == 0:
+            return 0
+
+        self.M = M
+        self._visited = [False] * self._n
+        count = 0
+        for i in range(self._n):
+            if not self._visited[i]:
+                self._dfs(i)
+                count += 1
+        return count
+
+    def _dfs(self, i):
+        self._visited[i] = True
+        for j in range(self._n):
+            if i != j and not self._visited[j] and self.M[i][j] == 1:
+                self._dfs(j)
+
+s = Solution()
+print(s.findCircleNum([[1,1,0],[1,1,0],[0,0,1]]))
---
+++ Graph/Modeling/lc733.py
@@ -0,0 +1,27 @@
+﻿class Solution:
+    dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        if image[sr][sc] == newColor:
+            return image
+        self._r = len(image)
+        self._c = len(image[0])
+        self._newColor = newColor
+        self._image = image
+        self._color = image[sr][sc]
+        self._visited = [[False for _ in range(self._c)] for _ in range(self._r)]
+
+        self._dfs(sr, sc)
+        return image
+
+    def _dfs(self, x, y):
+        self._visited[x][y] = True
+        self._image[x][y] = self._newColor
+
+        for d in Solution.dirs:
+            nx, ny = x + d[0], y + d[1]
+            if self._inAdge(nx, ny) and not self._visited[nx][ny] and self._image[nx][ny] == self._color:
+                self._dfs(nx, ny)
+
+    def _inAdge(self, x, y):
+        return x >= 0 and x < self._r and y >= 0 and y < self._c
+
