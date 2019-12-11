751a8d51e0366c25802c792385bf8c2b02a7d6c0
lidongdong <lidongdong_xynu@163.com>
2019-12-11 11:13:13 +0800

    lc

--- Graph/ModelBfs/lc1091.py
+++ Graph/ModelBfs/lc1091.py
@@ -1,1 +1,39 @@
-﻿
+﻿from collections import deque
+class Solution:
+
+    dirs = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] == 1:
+            return -1
+        if grid[n - 1][n - 1] == 1:
+            return -1
+        if n == 1:
+            return 1
+
+        self._n = n
+        self._visited = [[False for _ in range(n)] for _ in range(n)]
+        self._order = [[-1 for _ in range(n)] for _ in range(n)]
+        self._grid = grid
+
+        self._bfs(0, 0)
+        return self._order[n-1][n-1]
+
+    def _bfs(self, x, y):
+        self._visited[x][y] = True
+        queue = deque()
+        queue.append((x, y))
+        self._order[x][y] = 1
+
+        while len(queue) > 0:
+            curx, cury = queue.popleft()
+            for d in Solution.dirs:
+                nx, ny = curx + d[0], cury + d[1]
+                if self._inArea(nx, ny) and not self._visited[nx][ny] and self._grid[nx][ny] == 0:
+                    self._visited[nx][ny] = True
+                    self._order[nx][ny] = self._order[curx][cury] + 1
+                    queue.append((nx, ny))
+
+    def _inArea(self, x, y):
+        return x >= 0 and x < self._n and y >= 0 and y < self._n
--- Graph/ModelBfs/lc752.py
+++ Graph/ModelBfs/lc752.py
@@ -1,1 +1,48 @@
-﻿
+﻿from collections import deque
+class Solution:
+    def openLock(self, deadends, target: str) -> int:
+        self._deadends = set(deadends)
+        if target in self._deadends:
+            return -1;
+        if "0000" in self._deadends:
+            return -1;
+        if target == "0000":
+            return 0;
+
+        self._visited = dict()
+        self._target = target
+        self._bfs("0000")
+        
+
+        if target in self._visited:
+            return self._visited[target]
+        return -1
+
+    def _bfs(self, s):
+        self._visited[s] = 0
+        queue = deque()
+        queue.append(s)
+
+        while len(queue) > 0:
+            curr = queue.popleft()
+            nexts = self._getNexts(curr)
+            for n in nexts:
+                if n not in self._visited and n not in self._deadends:
+                    self._visited[n] = self._visited[curr] + 1
+                    queue.append(n)
+                    if n == self._target:
+                        return
+    
+                    
+    def _getNexts(self, s):
+        nexts = list()
+        for i in range(4):
+            v  = int(s[i])
+            n1 = s[:i] + str((v+1)%10) + s[i+1:]
+            n2 = s[:i] + str((v+9)%10) + s[i+1:]
+            nexts.append(n1);
+            nexts.append(n2);
+        return nexts
+
+s = Solution()
+print(s.openLock(["0201","0101","0102","1212","2002"], "0202"))
